83268fc60265e19a24a294ca68ea7e038b8ecd55
Fahmi <121919479+Fahmi-XD@users.noreply.github.com>
2023-10-07 10:05:59 +0700

    Add files via upload

---
+++ vendor/main.js
@@ -0,0 +1,9 @@
+$("document").ready(function () {
+    $("#reg").click(() => {
+        $(".box-blur").addClass("reg-page");
+    });
+    
+    $("#log").click(() => {
+        $(".box-blur").removeClass("reg-page");
+    });
+});
