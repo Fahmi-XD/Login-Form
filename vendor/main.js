286f7492f00f87fd1ebc78f240356525d7f6f04a
Fahmi <121919479+Fahmi-XD@users.noreply.github.com>
2023-10-07 16:36:40 +0700

    Add files via upload

--- vendor/main.js
+++ vendor/main.js
@@ -2,7 +2,7 @@ $("document").ready(function () {
     $("#reg").click(() => {
         $(".box-blur").addClass("reg-page");
     });
-    
+
     $("#log").click(() => {
         $(".box-blur").removeClass("reg-page");
     });
